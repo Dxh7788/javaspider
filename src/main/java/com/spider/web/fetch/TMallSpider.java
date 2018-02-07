58bc325b444bc2ba110de75a9d2128e655c1740b
Dxh7788 <anan1215>
2018-02-07 13:53:42 +0800

    op

---
+++ src/main/java/com/spider/web/fetch/TMallSpider.java
@@ -0,0 +1,10 @@
+package com.spider.web.fetch;
+
+/**
+ * Copyright (C) 2017-2018 https://www.htouhui.com - A project by javaspider
+ *
+ * @author xh.d
+ * @since 2018/2/7 13:45
+ */
+public class TMallSpider {
+}
